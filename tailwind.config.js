583b3cd3804edc0ea5a1fdd2da1232a6b7d18e0b
Felix <felix.aberham1@gmail.com>
2024-10-16 22:37:48 +0200

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+}
+
