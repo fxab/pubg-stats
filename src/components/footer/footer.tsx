6418c8349a2aba1d8bc7e85d46a701920b93b4ef
Felix <felix.aberham1@gmail.com>
2024-10-17 19:02:14 +0200

    feat: create footer layout

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <footer class="footer p-10 bg-neutral text-neutral-content">
+      <div class="flex justify-center">
+        <span>Made with ♡ by CRYSTALOPS</span>
+      </div>
+    </footer>
+  );
+});
