6937016c0511ec10824a2e13384b27d67ee596c0
Felix <felix.aberham1@gmail.com>
2024-10-18 15:10:47 +0200

    feat: define pubg api key

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,9 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    define: {
+      'process.env.PUBG_API_KEY': JSON.stringify(process.env.PUBG_API_KEY),
+    },
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
       // Put problematic deps that break bundling here, mostly those with binaries.
